c04a90249bee43f56bfbcbb401774f10a1e0d540
SaraToth <saratoth101@gmail.com>
2024-10-15 15:50:23 +0900

    Continue the logic for mergeSort

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 const myArray = [3, 2, 1, 13, 8, 5, 0, 1];
 
 function mergeSort(array) {
-    if(array.length <= 1) return array;
+    if(array.length <= 1 ) {
+        return array;
+    }
+    if(array.length > 1) {
+        const halfLength = array.length / 2;
+        const leftHalf = array.splice(0, halfLength);
+        const rightHalf = array;
+        
+        const sortedLeft = mergeSort(leftHalf);
+        const sortedRight = mergeSort(rightHalf);
 
-    const halfLength = array.length / 2;
-    const leftHalf = array.splice(0, halfLength);
-    const rightHalf = array
-    
+
+    }
+    return merge(sortedLeft, sortedRight);
 }
+
+function merge(left, right) {
+    const result = [];
+
+    i = 0;
+    j = 0;
+
+    while (i < left.length || j < right.length) {
+        if(left[i] < right[j]) {
+            result.push(left[i]);
+            i++;
+        } else if(right[j] < left[i]) {
+            result.push(right[j]);
+            j++;
+        }
+    }
+
+    return result;
+}
+
+console.log(mergeSort(myArray));
