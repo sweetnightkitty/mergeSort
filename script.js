acfae889e8c85e1bc361c05014021f9edca2cf3e
SaraToth <saratoth101@gmail.com>
2024-10-15 15:11:00 +0900

    Create html and js files, create myArray and basic function of mergeSort to split array in half

---
+++ script.js
@@ -0,0 +1,10 @@
+const myArray = [3, 2, 1, 13, 8, 5, 0, 1];
+
+function mergeSort(array) {
+    if(array.length <= 1) return array;
+
+    const halfLength = array.length / 2;
+    const leftHalf = array.splice(0, halfLength);
+    const rightHalf = array
+    
+}
