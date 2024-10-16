fa817bd329b68b1d298e0bbfdc6807c9abb043da
SaraToth <saratoth101@gmail.com>
2024-10-16 16:30:14 +0900

    Finish logic, and add explanatory notes for how mergeSort works

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
 const myArray = [3, 2, 1, 13, 8, 5, 0, 1];
 
 function mergeSort(array) {
-    if(array.length <= 1 ) {
+    //Recursive base case
+    if(array.length === 1 ) {
         return array;
     }
-    if(array.length > 1) {
-        const halfLength = array.length / 2;
-        const leftHalf = array.splice(0, halfLength);
-        const rightHalf = array;
-        
-        const sortedLeft = mergeSort(leftHalf);
-        const sortedRight = mergeSort(rightHalf);
 
+    //Divides all even arrays in half; for uneven arrays math.floor rounds down (3/2 -> 1.5 becomes 1)
+    const halfLength = Math.floor(array / 2);
+    const left = array.slice(0, halfLength);
+    const right = array.slice(halfLength);
 
-    }
-    return merge(sortedLeft, sortedRight);
+    //recursively calls mergeSort on both the left and right halves
+    //mergeSort halves the arrays until the base case (length = 1) is met
+    //Then merge can finally be calculated, and each merge call is calculated thereafter
+
+    return merge(mergeSort(left), mergeSort(right));
 }
 
+
 function merge(left, right) {
-    const result = [];
+    let result = [],
+        leftIndex = 0,
+        rightIndex = 0;
 
-    i = 0;
-    j = 0;
-
-    while (i < left.length || j < right.length) {
-        if(left[i] < right[j]) {
-            result.push(left[i]);
-            i++;
-        } else if(right[j] < left[i]) {
-            result.push(right[j]);
-            j++;
+    //While loops ends when the array[index] of ONE array is empty
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if(left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
         }
     }
 
-    return result;
+    //The array with the smallest values when slice[index] returns nothing
+    //The array with the largest values that have not been sorted are sliced & concat
+    return result
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
 }
 
 console.log(mergeSort(myArray));
